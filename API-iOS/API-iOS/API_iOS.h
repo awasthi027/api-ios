9d9c1f37523e0475e4bf37530d71f23402ae8764
Ashish Awasthi <ashisha2@vmware.com>
2024-03-07 14:21:52 +0530

    Created podspec file

---
+++ API-iOS/API-iOS/API_iOS.h
@@ -0,0 +1,18 @@
+//
+//  API_iOS.h
+//  API-iOS
+//
+//  Created by Ashish Awasthi on 07/03/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for API_iOS.
+FOUNDATION_EXPORT double API_iOSVersionNumber;
+
+//! Project version string for API_iOS.
+FOUNDATION_EXPORT const unsigned char API_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <API_iOS/PublicHeader.h>
+
+
---
+++ API-iOS/API-iOS/APIs/TestClass.swift
@@ -0,0 +1,18 @@
+//
+//  TestClass.swift
+//  API-iOS
+//
+//  Created by Ashish Awasthi on 07/03/24.
+//
+
+import Foundation
+
+public class TestClass {
+
+    public init() { /* No Action */}
+
+    public func completeName(firstName: String, lastName: String) -> String {
+         debugPrint("Recevied call from outside module")
+        return firstName + lastName
+    }
+}
