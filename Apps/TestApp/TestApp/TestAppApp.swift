9254ac019651f3734ee473e46e092ea75cfe3b02
A <A>
2024-12-18 18:15:44 +0530

    Added compiled sample appe

---
+++ Apps/TestApp/TestApp/TestAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Ashish Awasthi on 18/12/24.
+//
+
+import SwiftUI
+import API_iOS
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apps/TestApp/TestAppTests/TestClassTests.swift
@@ -0,0 +1,40 @@
+//
+//  TestClassTests.swift
+//  API_iOSTests
+//
+//  Created by Ashish Awasthi on 01/06/24.
+//
+
+import XCTest
+import API_iOS
+
+final class TestClassTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+    func testGetFullNameSuccess() {
+        let obj: TestClass = TestClass()
+        let fullName = obj.completeName(firstName: "Ashish", 
+                                        lastName: "Awasthi")
+        XCTAssertEqual(fullName, "AshishAwasthi")
+    }
+
+    func testGetFullNameFail() {
+        let obj: TestClass = TestClass()
+        let fullName = obj.completeName(firstName: "Ashish", 
+                                        lastName: "Awasthi")
+        XCTAssertNotEqual(fullName, "Ashish Awasthi")
+    }
+}
